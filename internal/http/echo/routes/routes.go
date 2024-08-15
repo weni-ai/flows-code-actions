3e1dd05e392d852dc465d87a5addd8a0975d2702
Rafael Soares <rafael.dvlpr@gmail.com>
2024-08-15 18:23:53 -0300

    add route to update code with PATCH /admin/code/:codeid

--- internal/http/echo/routes/routes.go
+++ internal/http/echo/routes/routes.go
@@ -40,6 +40,7 @@ func Setup(server *s.Server) {
 	server.Echo.GET("/health", healthHandler.Health)
 
 	server.Echo.POST("/admin/code", handlers.ProtectEndpointWithAuthToken(server.Config, codeHandler.CreateByAdmin))
+	server.Echo.PATCH("/admin/code/:id", handlers.ProtectEndpointWithAuthToken(server.Config, codeHandler.UpdateByAdmin))
 	server.Echo.POST("/code", handlers.ProtectEndpointWithAuthToken(server.Config, codeHandler.Create))
 	server.Echo.GET("/code", handlers.ProtectEndpointWithAuthToken(server.Config, codeHandler.Find))
 	server.Echo.GET("/code/:id", handlers.ProtectEndpointWithAuthToken(server.Config, codeHandler.Get))
