f82010d203ae00020b8f50aac4721d15073ae983
Rafael Soares <rafael.dvlpr@gmail.com>
2024-09-20 10:51:07 -0300

    add some default libs to avoid be install on ExtractPythonLibs

--- internal/codelib/codelib.go
+++ internal/codelib/codelib.go
@@ -53,7 +53,7 @@ func NewCodeLib(name string, language LanguageType) *CodeLib {
 }
 
 func ExtractPythonLibs(pythonCode string) []string {
-	standardLibraries := []string{"base64", "datetime", "json", "math", "os", "random", "re", "sys"}
+	standardLibraries := []string{"base64", "datetime", "email", "imaplib", "json", "math", "os", "random", "re", "sys", "time", "urllib", "urllib.parse", "urllib.request"} // must be alphabetically ordered
 	re := regexp.MustCompile(`^(from|import)\s+([\w.]+)`)
 
 	var libraries []string
