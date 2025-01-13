466293938f723bcf04348b3dd7ef882af4dc0c90
Rafael Soares <rafael.dvlpr@gmail.com>
2025-01-13 17:16:14 -0300

    test blacklist

---
+++ internal/code/code_test/service_test.go
@@ -0,0 +1,93 @@
+package code_test
+
+import (
+	"context"
+	"log"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/weni-ai/flows-code-actions/config"
+	"github.com/weni-ai/flows-code-actions/internal/code"
+	coderepos "github.com/weni-ai/flows-code-actions/internal/code/mongodb"
+	"github.com/weni-ai/flows-code-actions/internal/codelib"
+	librepos "github.com/weni-ai/flows-code-actions/internal/codelib/mongodb"
+	"github.com/weni-ai/flows-code-actions/internal/db"
+)
+
+func TestCreateCodeWithBlackList(t *testing.T) {
+	os.Setenv("FLOWS_CODE_ACTIONS_BLACKLIST", "foo,bar,baz,qux")
+
+	cfg := config.NewConfig()
+
+	db, err := db.GetMongoDatabase(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	libRepo := librepos.NewCodeLibRepo(db)
+	libService := codelib.NewCodeLibService(libRepo)
+
+	repo := coderepos.NewCodeRepository(db)
+	codeService := code.NewCodeService(cfg, repo, libService)
+
+	_, err = codeService.Create(context.TODO(), &code.Code{
+		Name:        "Test Code",
+		Source:      "foo bar baz qux",
+		Language:    code.TypePy,
+		Type:        code.TypeEndpoint,
+		URL:         "https://example.com",
+		ProjectUUID: "5e82df29-f731-4861-8836-1b047ce03506",
+	})
+	assert.Equal(t, err.Error(), "source code contains blacklisted term")
+
+	cd, err := codeService.Create(context.TODO(), &code.Code{
+		Name:        "Test Code",
+		Source:      "def Run(engine):\nprint('ahoy')",
+		Language:    code.TypePy,
+		Type:        code.TypeEndpoint,
+		URL:         "https://example.com",
+		ProjectUUID: "5e82df29-f731-4861-8836-1b047ce03506",
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, "Test Code", cd.Name)
+
+}
+
+func TestUpdateCodelibWithBlackList(t *testing.T) {
+	os.Setenv("FLOWS_CODE_ACTIONS_BLACKLIST", "foo,bar,baz,qux")
+
+	cfg := config.NewConfig()
+
+	db, err := db.GetMongoDatabase(cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	libRepo := librepos.NewCodeLibRepo(db)
+	libService := codelib.NewCodeLibService(libRepo)
+
+	repo := coderepos.NewCodeRepository(db)
+	codeService := code.NewCodeService(cfg, repo, libService)
+
+	cd, _ := codeService.Create(context.TODO(), &code.Code{
+		Name:        "Test Code",
+		Source:      "def Run(engine):\nprint('ahoy')",
+		Language:    code.TypePy,
+		Type:        code.TypeEndpoint,
+		URL:         "https://example.com",
+		ProjectUUID: "5e82df29-f731-4861-8836-1b047ce03506",
+	})
+
+	_, err = codeService.Update(context.TODO(), cd.ID.String(), "Test Code", "foo bar baz qux", string(code.TypeEndpoint))
+
+	assert.Equal(t, err.Error(), "source code contains blacklisted term")
+
+	id := cd.ID.Hex()
+	cdu, err := codeService.Update(context.TODO(), id, "Test Code", "def Run(engine):\nprint('ahoy2')", string(code.TypeEndpoint))
+
+	assert.NoError(t, err)
+	assert.True(t, strings.Contains(cdu.Source, "ahoy2"))
+}
